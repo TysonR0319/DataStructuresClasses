8f3fa041c7e71821c61fd6e93d8f4712fb31b0a0
TysonR0319 <tysonr0319@gmail.com>
2022-11-26 11:15:20 -0500

    SinglyLinkedList Class and push method

---
+++ index.js
@@ -0,0 +1,29 @@
+// Singly Linked List 
+//piece of data - val
+//reference to next node - next
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SInglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode
+        }
+        this.length++;
+        return this;
+    }
+}
